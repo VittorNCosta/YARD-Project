e5366c1fb5d469c9a5151a4f4fb687b8babf4e02
VittorNCosta <vittor.hy@gmail.com>
2026-02-16 10:43:02 -0300

    Adicionado VITE + TS

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-
+ 
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("🔥 MongoDB conectado com sucesso!");
+    console.log("📦 DB:", mongoose.connection.name);
   } catch (error) {
     console.error("❌ Erro ao conectar ao MongoDB:", error);
     process.exit(1);
